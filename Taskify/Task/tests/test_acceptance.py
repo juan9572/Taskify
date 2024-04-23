f4bbc9343235273e3826871aa786c29947da6e8b
Juan Pablo Rincon Usma <jprinconu@eafit.edu.co>
2024-04-23 13:10:50 -0500

    Refactor some code

--- Taskify/Task/tests/test_acceptance.py
+++ Taskify/Task/tests/test_acceptance.py
@@ -12,13 +12,11 @@ class TaskAcceptanceTest(LiveServerTestCase):
         cls.playwright = sync_playwright().start()
         cls.browser = cls.playwright.chromium.launch()
 
-
     @classmethod
     def tearDownClass(cls):
         super(TaskAcceptanceTest, cls).tearDownClass()
         cls.browser.close()
         cls.playwright.stop()
-
 
     def test_task_crud(self):
         self.user = User.objects.create_user(
@@ -32,35 +30,28 @@ class TaskAcceptanceTest(LiveServerTestCase):
         page.get_by_role("button", name="Logueate").click()
         expect(page.get_by_role("heading", name="Hola Testuser")).to_be_visible()
         page.get_by_role("link", name="tarea").click()
-
         # Create Task
         page.locator("#id_title").fill("New Task")
         page.locator("#id_description").fill("New Description")
         page.get_by_role("button", name="Guardar").click()
-
         # Verify Task Created
         expect(page.locator("#tasklist")).to_contain_text("New Task")
         expect(page.locator("h3")).to_contain_text("Tienes 1 tarea incompleta")
-
         # Update Task
         page.get_by_role("link", name="New Task").click()
         page.locator("#id_title").fill("Updated Task")
         page.locator("#id_description").fill("Updated Description")
         page.locator("#id_complete").check()
         page.get_by_role("button", name="Guardar").click()
-
         # Verify Task Updated
         expect(page.locator("h3")).to_contain_text("Tienes 0 tareas incompletas")
         expect(page.locator("#tasklist")).to_contain_text("Updated Task")
-
         # Delete Task
         page.get_by_role("link", name="×").click()
         page.get_by_role("button", name="Eliminar").click()
-
         # Verify Task Deleted
         expect(page.locator("#tasklist")).to_contain_text("No hay tareas nuevas.")
         expect(page.locator("#tasklist")).to_contain_text("Crea una nueva tarea !")
-
         # Logout
         page.get_by_role("button", name="Cerrar sesión").click()
         page.close()
--- Taskify/User/tests/test_acceptance.py
+++ Taskify/User/tests/test_acceptance.py
@@ -18,19 +18,16 @@ class UserAuthenticationTest(LiveServerTestCase):
         cls.browser.close()
         cls.playwright.stop()
 
-
     def test_user_registration(self):
         page = self.browser.new_page()
         page.goto(self.live_server_url)
         # Navigate to Registration Page
         page.get_by_role("link", name="Registrate").click()
-
         # Register
         page.locator("#id_username").fill("newuser")
         page.locator("#id_password1").fill("newpasswordHiperSecret123")
         page.locator("#id_password2").fill("newpasswordHiperSecret123")
         page.get_by_role("button", name="Registrarse").click()
-
         # Verify Successful Registration
         expect(page.locator("h1")).to_contain_text("Hola Newuser")
         expect(page.get_by_role("button", name="Cerrar sesión")).to_be_visible()
@@ -43,12 +40,10 @@ class UserAuthenticationTest(LiveServerTestCase):
         User.objects.create_user(
             username="newuser", password="newpasswordHiperSecret123"
         )
-
         # Login
         page.locator("#id_username").fill("newuser")
         page.locator("#id_password").fill("newpasswordHiperSecret123")
         page.get_by_role("button", name="Logueate").click()
-
         # Verify Successful Login
         expect(page.locator("h1")).to_contain_text("Hola Newuser")
         expect(page.get_by_role("button", name="Cerrar sesión")).to_be_visible()
